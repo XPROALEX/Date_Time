7d47ff3bdbfd7b9c54429f6d95c693740eacb28f
Alex <alex97av@gmail.com>
2023-07-27 15:49:35 +0200

    fine esercizio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args){
-
+        LocalDate today= LocalDate.now();
+        LocalDate endOfMonth = today.withDayOfMonth(today.lengthOfMonth());
+        LocalDate endOfYear=today.withDayOfYear(today.lengthOfYear());
+        int daysLefEndOfMonth = endOfMonth.lengthOfMonth()-today.getDayOfMonth();
+        int dayLeftEndOfYear = (endOfYear.lengthOfYear()-today.getDayOfYear());
+        System.out.println("Days left till the end of the month= "+daysLefEndOfMonth);
+        System.out.println("Days left till the end of the year= "+dayLeftEndOfYear);
     }
 }
